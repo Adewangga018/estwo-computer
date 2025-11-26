d555360451046194c919293da048083135837367
Adewangga018 <arjuna.dewangga018@gmail.com>
2025-11-26 14:53:17 +0700

    Fix Mobile Display Name

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'Estwo Computer'),
     // Tambahan konfigurasi untuk password admin
     'admin_password' => env('ADMIN_PASSWORD', 'Bismillah212'),
 
